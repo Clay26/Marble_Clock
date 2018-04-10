7275d0323db3c526c2dbdaa2c4059286e3076882
Clay Jackson <clayjackson@Clays-MacBook-Pro.local>
2018-04-10 10:07:37 -0400

    Initial commit of main project file.
    ME 5194, Smart Products, final prototype project.
    Creating a marble machine clock that tracks the current hour using the
    DS3231 RTC module.

---
+++ clock.cpp
@@ -0,0 +1,21 @@
+// File: clock.cpp
+// Created by: Clay Jackson
+// Co-Creator: Carter Hurd
+// Created on: 04/10/2018
+// Synopsis: 
+//   A prototype project a part of the ME 5194, Smart Products, class.
+//   Goal: Create a marble machine that tracks the current hour using
+//   the DS3231 RTC module.
+//   Please see consult additional project files for visuals.
+
+using namespace std;
+
+int main() {
+
+
+
+
+
+
+  return 1;
+}
